ed000dd5d882a788e60f5fe4f54b1efc476817b7
Raymond Mwebe <122720713+Raymondcoder27@users.noreply.github.com>
2024-08-05 12:03:01 +0300

    Update taskStore.js

--- src/stores/taskStore.js
+++ src/stores/taskStore.js
@@ -5,6 +5,7 @@ export const useTaskStore = defineStore('taskStore', {
     tasks: [
       {id: 1, title: "buy milk", isFav: false},
       {id: 2, title: "play fifa", isFav: true}
+      {id: 3, title: "mow lawn", isFav: true}
     ], 
     name: 'Raymond'
   }),
